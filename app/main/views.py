290e7d7f4291e52543deebfafab44426f64c13d1
Jun Luo <luojl@amazon.com>
2017-07-08 15:12:54 +0800

    1. add flash message in html 2. add active effect in navbar item

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,21 @@
-from flask import render_template
+from flask import render_template, flash
 from . import main
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@main.route('/notes')
+def notes():
+    return render_template('notes.html')
+
+@main.route('/tags')
+def tags():
+    return render_template('tags.html')
 
 @main.route('/about')
 def about():
