6ae2b9ba48ab970a2cd54255059f360593fa0ce9
Jun Luo <luojl@amazon.com>
2017-07-09 10:36:03 +0800

    add several endpoints and .html

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,9 @@
 from flask import render_template, flash
 from . import main
+from manage import app
+
+with app.app_context(): #TODO ugly, another way?
+    from ..models import User
 
 @main.route('/')
 def index():
@@ -20,3 +24,11 @@ def tags():
 @main.route('/about')
 def about():
     return render_template('about.html')
+
+@main.route('/login')
+def login():
+    return render_template('login.html')
+
+@main.route('/register')
+def register():
+    return render_template('register.html')
