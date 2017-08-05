44d5ac88a5ee87e26d08a1518022dd073581f4d0
Jun Luo <luojdev@gmail.com>
2017-08-05 18:40:44 +0800

    Add method to purge all data in dev db

--- manage.py
+++ manage.py
@@ -7,12 +7,19 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 
 with app.app_context():
-    from app.models import User, Note, Project, Comment, Tag
+    from app.models import User, Note, Tag
 
 def make_shell_context():
-    return dict(app=app, mongo=mongo, User=User, Note=Note, Project=Project,
-                Comment=Comment, Tag=Tag)
+    return dict(app=app, mongo=mongo, User=User, Note=Note, Tag=Tag)
 manager.add_command("shell", Shell(make_context=make_shell_context))
+
+@manager.command
+def delete_all_db():
+    if app.config['MONGO_DBNAME'] != 'Development':
+        raise ValueError("only development db deletes all data")
+    User.collection.delete_many({})
+    Note.collection.delete_many({})
+    Tag.collection.delete_many({})
 
 if __name__ == '__main__':
     manager.run()
